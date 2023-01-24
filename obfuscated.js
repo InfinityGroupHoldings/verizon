bc2a9542c84d1244d58cae894c4d372d7678cd00
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-01-24 13:16:42 -0500

    ...

--- obfuscated.js
+++ obfuscated.js
@@ -1,19 +1,13 @@
-import {pass1, pass2} from './key1.js'
+import { pass1, pass2 } from './key1.js'
 
 const crypto = require('crypto');
 
-const algorithm = 'aes-256-cbc';
-const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
+const password = pass1 + "RqcEqmC8m2BZ5dPd3phV9mAGyVQcqzN288fk8PjCttKznmP3xPMZYqcbxBpk" + pass2
 
-function decrypt(text) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
-    let decipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
-}
+let decryptedData = "";
 
-let data = decrypt("hello world!");
-console.log(decrypt(data)); // Outputs "hello world!"
+const decipher = crypto.createDecipher("aes-256-cbc", password);
+
+decryptedData += decipher.update(encryptedData, "hex", "utf-8")
+
+decryptedData += decipher.final('utf-8')
